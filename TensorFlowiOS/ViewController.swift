676745986b50d90768c54cab42ca771d17e17b39
Max <maximilian.fehrentz@envidual.com>
2020-09-29 15:58:58 +0200

    Initial Commit

---
+++ TensorFlowiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TensorFlowiOS
+//
+//  Created by Max on 29.09.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
